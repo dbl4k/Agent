d1e4dd64b6a8b373161aa8707a563e291d0b2b4c
dbl4k <github@dbl4k.net>
2017-02-12 15:53:02 +0000

    Added E_FatalUNHANDLED exception message

--- Agent.Common/Messages.vb
+++ Agent.Common/Messages.vb
@@ -2,4 +2,5 @@
     Public Const E_FINDTASK_TOOMANYMATCHES As String = "Too many matches for name '{0}' (found {1} in total). Consider using a fully resolved type name instead."
     Public Const E_FINDTASK_NOMATCHES As String = "No matches for name '{0}'. Check the name is a valid class name and that the class inherits from TaskAgent."
     Public Const E_FINDTASK_FOUNDTASK As String = "Found task '{0}'."
+    Public Const E_FATALUNHANDLED As String = "An unhandled fatal exception occurred in Agent Task {0}. See inner exception for details."
 End Class

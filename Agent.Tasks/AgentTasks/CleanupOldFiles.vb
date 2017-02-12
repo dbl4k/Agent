ca9db9f3aa8d8fd1c4b46d735fc8f457e53c7ba8
dbl4k <github@dbl4k.net>
2017-02-12 15:59:35 +0000

    CleanupOldFiles - removed unnecccessary exceptions

--- Agent.Tasks/AgentTasks/CleanupOldFiles.vb
+++ Agent.Tasks/AgentTasks/CleanupOldFiles.vb
@@ -2,14 +2,8 @@
 
     Protected Overrides Sub Run()
         Console.WriteLine("Cleaning up old files")
-        throwUneccessaryExceptions()
         System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2).TotalMilliseconds)
-
     End Sub
 
-    Sub throwUneccessaryExceptions()
-        RecordError(New EntryPointNotFoundException("Oh wow"), "this is a sample")
-        Throw New IndexOutOfRangeException("OMG!")
-    End Sub
 
 End Class
